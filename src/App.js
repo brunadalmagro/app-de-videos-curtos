b9a306c20c40ff12874e0f971c794a5faa310c5a
Bruna D <bruna.dalmagro23re20@gmail.com>
2023-06-19 23:53:38 -0300

    aula 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Videos from "./Pages/Video";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <div className="App">
+    <div className="app__videos">
+    <Videos />
+    <Videos />
+    <Videos />
     </div>
-  );
+  </div>;
 }
 
 export default App;
