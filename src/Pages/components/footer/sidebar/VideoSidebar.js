3107e79aa6516e377f98907e1b6be99df5dd9b9f
Bruna D <bruna.dalmagro23re20@gmail.com>
2023-06-20 19:19:09 -0300

    Interação de like/deslike
    ## O que é a alteração
    Foram feitas alterações no repositório 'VideoSidebar.js' adiocionando uma função'tracking' e condição ternária de contagem de 'likes', 'like/deslike'.

--- src/Pages/components/footer/sidebar/VideoSidebar.js
+++ src/Pages/components/footer/sidebar/VideoSidebar.js
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./VideoSidebar.css";
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ChatIcon from '@mui/icons-material/Chat';
-import ShareIcon from '@mui/icons-material/Share';
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ChatIcon from "@mui/icons-material/Chat";
+import ShareIcon from "@mui/icons-material/Share";
 
 function VideoSidebar() {
-  return <div className="videoSidebar">
-    <div className="videoSidebar__options">
-    <FavoriteBorderIcon fontSize="medium"/>
-      <p>1365 </p>
+
+    const [liked, setLiked] = useState(false)
+
+    function handleLike(){
+        setLiked(!liked)
+    }
+
+  return (
+    <div className="videoSidebar">
+      <div 
+      className="videoSidebar__options"
+      onClick={handleLike}
+      >
+        { liked ? <FavoriteIcon fontSize="medium"/> :         <FavoriteBorderIcon fontSize="medium" /> }
+
+        <p> {liked ? 820 + 1 : 300} </p>
+      </div>
+
+      <div className="videoSidebar__options">
+        <ChatIcon fontSize="medium" />
+        <p>200 </p>
+      </div>
+
+      <div className="videoSidebar__options">
+        <ShareIcon fontSize="medium" />
+        <p>800 </p>
+      </div>
     </div>
-    
-    <div className="videoSidebar__options">
-      <ChatIcon fontSize="medium"/>
-      <p>200 </p>
-    </div>
-    
-    <div className="videoSidebar__options">
-      <ShareIcon fontSize="medium"/>
-      <p>800 </p>
-    </div>
-    </div>;
+  );
 }
 
 export default VideoSidebar;
